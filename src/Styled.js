89adbb8a9e06a4f45f0204ad3fcc3691d079b380
vagrant <vagrant@vagrant.vm>
2019-11-27 21:19:56 +0000

    add tag-based styled components

---
+++ src/Styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const StyledForm = styled.form`
+    background-color: papayawhip;
+    box-shadow: 0px 0px 5px gray;
+    padding: 10px;
+    width: 300px;
+    `;
