c0f396bcade2b898bc1654c0bb5d2ecd69f86bf7
vagrant <vagrant@vagrant.vm>
2019-11-28 05:57:18 +0000

    lifted state up to App from MovieList

--- src/MovieList.js
+++ src/MovieList.js
@@ -3,33 +3,9 @@ import styled from 'styled-components';
 import StyledTitle from './Title';
 import StyledMovie from './Movie';
 
-		//	Class Stateful Components
-        class MovieList extends React.Component {
-            constructor(props){
-                super(props);
-                this.state = {
-                    movies : []
-                };
-            }
-            componentDidMount() {
-              (async () => {
-                try {
-                  // Make an API Request and store the Response
-                  const response = await fetch('http://192.168.33.10:3004/movies');
-                  // This is for HTTP Errors, not Networking Errors
-                  if (!response.ok) throw Error(response.status + ': ' + response.statusText);
-                  // extract the JSON from the body of the Response
-                  const result = await response.json();
-                  // update the state variable
-                  this.setState({
-                    movies: result
-                  });
-                } catch(error) {
-                  // This is for Networking Errors
-                  console.log('Fetch API Error: ' + error);
-                }
-              })();
-            } 
+		//	Stateless Function Components
+        const MovieList = ({className, movies}) => {
+            
     
             render() {
                 return (
